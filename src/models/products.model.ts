3ffab4e855860f68d9255f55952a0c72adcd0488
Abdul Aziz <abdulaziz758015@gmail.com>
2025-12-29 21:38:00 +0600

    complete product model

--- src/models/products.model.ts
+++ src/models/products.model.ts
@@ -11,3 +11,35 @@ interface IProduct {
   createdAt?: Date;
   updatedAt?: Date;
 }
+
+const productSchema = new mongoose.Schema<IProduct>({
+    name: {
+        type: String,
+        required: true},
+
+    price: {
+        type: Number,
+        required: true},
+
+    category: {
+        type: String,
+        enum: ["fruits", "vegetables", "dairy", "bakery", "beverages", "snacks", "household", "personal_care","skincare","beauty", "others"],
+        required: true},
+
+    imageUrl: {
+        type: String,
+        required: true},
+    unit: {
+        type: String,
+        required: true},
+    stock: {
+        type: Number,
+        required: true,
+        default: 0}
+
+
+},{timestamps:true});
+
+const Product = mongoose.models.Product || mongoose.model("Product", productSchema);
+
+export default Product;
